2daa3b8b591e89bde83ab4d77004d5ebbbe9bfa2
nirmalprajapat615 <nirmalprajapat7834@gmail.com>
2025-01-07 18:43:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const contactSection = document.querySelector("#contact");
+
+    contactSection.addEventListener("click", () => {
+        alert("Thank you for visiting the Brahman Course website!");
+    });
+});
